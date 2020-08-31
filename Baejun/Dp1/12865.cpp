504c898543e85029903cfd611bf21967433048a2
박기선 <arkingco@gmail.com>
2020-08-31 11:35:10 +0000

    공부 조금..

---
+++ Baejun/Dp1/12865.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int n , w ,k;
+int dp[100][100001];
+vector<pair<int,int>> valuable;
+int main(){
+	cin >> n >> k;
+	for(int i=0; i<n; ++i){
+		int W , V;
+		cin >> W >> V;
+		valuable.push_back(make_pair(W,V));
+	}	
+	
+	for(int i=1; i<=n; ++i){
+		for(int j=0; j<=k; ++j){
+			if(j>valuable[i].first) dp[i][j]=dp[i-1][j];
+            else dp[i][j] = max(dp[i-1][j] , dp[i-1][j-valuable[i].first] + valuable[i].second);	
+			
+			cout << dp[i][j] << endl;
+		}
+	}
+}
--- Baejun/Dp1/1912.cpp
+++ Baejun/Dp1/1912.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
 using namespace std;
 
+int dp[100001];
+int arr[100001];
 int main(){
-	
+	int n;
+	cin >> n;
+	for(int i=0; i<n; ++i){
+		cin >> arr[i];
+	}
+	dp[0] = arr[0];
+	int maxval = dp[0];
+	for(int i=1; i<n; ++i){
+		
+		dp[i] = max(dp[i-1] + arr[i] , arr[i]);
+		maxval = max(maxval , dp[i]);
+		cout << "dp[i] : "<<dp[i] << " " << dp[i-1] + arr[i] << "  " << arr[i] << endl;
+	}
+	cout << maxval << endl;
 }
