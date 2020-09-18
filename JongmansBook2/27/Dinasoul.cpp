dc9133c4a40f73aed99070e304f141902586753f
박기선 <arkingco@gmail.com>
2020-09-18 09:59:36 +0000

    종만북 공부

--- JongmansBook2/27/Dinasoul.cpp
+++ JongmansBook2/27/Dinasoul.cpp
@@ -1,72 +1,78 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include<iostream>
+#include<vector>
+#include<string>
+#include<stack>
+#include<algorithm>
 using namespace std;
 
-vector<vector<int>> adj;
-vector<int> seen, order;
-
-void makeGraph(const vector<string>& words){
-    adj = vector<vector<int>>(26, vector<int>(26,0));
-    for(int j=1;j<words.size();j++){
-        int i=j-1, len = min(words[i].size(), words[j].size());
-        //word[j]가 word[i] 앞에 오는 이유를 찾는다.
-        for(int k=0;k<len;k++)
-            if(words[i][k] != words[j][k]){
-                int a = words[i][k] - 'a';
-                int b = words[j][k] - 'a';
-                adj[a][b] = 1;
-                break;
-            }
-    }
+const int MAX_VALUE = 26;
+int T , n;
+vector<vector<int> > value_graph;
+bool visited[MAX_VALUE+1];
+vector<int> V;
+void make_value_graph(vector<string>& input){
+	value_graph = vector<vector<int> >(26, vector<int>(26, 0));
+	for(int i=1; i<input.size(); ++i){
+		int j = i-1; 
+		int forLength = min(input[i].size() , input[j].size());
+		for(int k=0; k < forLength; ++k ){
+			if(input[j][k] != input[i][k]){
+				int a = input[j][k] - 'a';
+				int b = input[i][k] - 'a';
+				value_graph[a][b] = 1;
+				cout << a << " " << b << endl;
+				break;
+			}
+		}
+	}
 }
 
 void dfs(int here){
-    seen[here] = 1;
-    for(int there=0;there<adj.size();there++)
-        if(adj[here][there] && !seen[there])
-            dfs(there);
-    order.push_back(here);
+	visited[here] = 1;
+	for(int i=0; i<value_graph.size(); ++i){
+		if(visited[i] == 0 && value_graph[here][i] == 1){
+			cout << here << " " << i << "in " << endl;
+			dfs(i);
+		}
+			
+	}
+	V.push_back(here);
 }
 
-//adj에 주어진 그래프를 위상정렬한 결과를 반환한다.
-//그래프가 DAG가 아니라면 빈 벡터를 반환한다.
-vector<int> topologicalSort(){
-    int n = adj.size();
-    seen = vector<int>(n,0);
-    order.clear();
-    for(int i=0;i<n;i++)
-        if(!seen[i]) dfs(i);
-    reverse(order.begin(), order.end());
-
-    //만약 그래프가 DAG가 아니라면 정렬 결과에 역방향 간선이 있다.
-    for(int i=0;i<n;i++)
-        for(int j=i+1;j<n;j++)
-            if(adj[order[j]][order[i]])
-                return vector<int>();
-    //없는 경우라면 깊이 우선 탐색에서 얻은 순서를 반환한다.
-    return order;
+vector<int> constribut(){
+	
+	for(int i=0; i<MAX_VALUE; ++i) if(visited[i] == 0) dfs(i);
+	
+	reverse(V.begin() , V.end());
+	
+	for(int i=0; i<MAX_VALUE; ++i){
+		for(int k=i+1; k<MAX_VALUE; ++i){
+			if(value_graph[V[i]][V[k]] == 1)
+				return vector<int>();
+		}
+	}
+	return V;
 }
 
-int main( ) {
-    int T;
-    cin >> T;
-    while(T--){
-        int N;
-        cin >> N;
-        vector<string> words = vector<string>(N);
-        for(int i=0;i<N;i++)
-            cin >> words[i];
-        makeGraph(words);
-        vector<int> result = topologicalSort();
-        if(result.empty())
-            cout << "INVALID HYPOTHESIS" << endl;
-        else{
-            for(int i=0;i<result.size();i++)
-                cout<<(char)(result[i]+'a');
-            cout<<endl;
-        }
-    }
-    return 0;
+int main(){
+	
+	cin >> T;
+	while(T--){
+		cin >> n;
+		vector<string> input;
+		
+		while(n--){
+			string a;
+			cin >> a;
+			input.push_back(a);
+		}
+		make_value_graph(input);
+		vector<int> ae = constribut();
+		
+		for(int i=0 ; i<ae.size(); ++i){
+			cout << ae[i] << " ";
+		}
+		
+	}
+	
 }
