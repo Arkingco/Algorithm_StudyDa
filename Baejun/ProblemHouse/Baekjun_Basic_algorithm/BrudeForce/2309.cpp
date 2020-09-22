51d39ccf58677940310ea1b41b660153fd08efe1
박기선 <arkingco@gmail.com>
2020-09-22 13:59:49 +0000

    i hate nanjangs

--- Baejun/ProblemHouse/Baekjun_Basic_algorithm/BrudeForce/2309.cpp
+++ Baejun/ProblemHouse/Baekjun_Basic_algorithm/BrudeForce/2309.cpp
@@ -17,14 +17,7 @@ const int Nanjangs = 9;
 vector<int> brude(Nanjangs);
 
 int force(int n){
-	if(n == 9) return 0;
-	if( force(n-1 ) + brude[n] == 100){
-		cout << "find";
-	} 
-	if( force(n-1) == 100) {
-		cout << "find 2";
-	}
-		
+
 } 
 
 int main(){
