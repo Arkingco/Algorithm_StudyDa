e0dd5100152b4503472b1a8d135bb55f9497efe6
박기선 <arkingco@gmail.com>
2020-12-25 07:30:44 +0000

    RMQ문제를 푸는 구간트리 입니당

--- JongmansBook2/24/RMQ.cpp
+++ JongmansBook2/24/RMQ.cpp
@@ -2,6 +2,8 @@
 #include<vector>
 using namespace std;
 
+const int INT_MAX = 987654321;
+
 struct RMQ {
 	// 배열의 길이
 	int n;
@@ -21,9 +23,29 @@ struct RMQ {
 		int rightMin = init(array, mid+1, right, node*2 +1);
 		return rangeMin[node] = min(leftMin, rightMin);
 	}
+	
+	// node가 표현하는 범위 array[nodeLeft, nodeRight] 가 주어질 때;
+	// 이 범위와 array[left, right]의 교집합의 최소치를 계산한다.
+	int query(int left, int right, int node, int nodeLeft, int nodeRight) {
+		// 두 구간이 겹치지 않으면 아주 큰 값을 반환한다. : 무시됨
+		if(right < nodeLeft || nodeRight < left) return INT_MAX;
+		// node가 표현하는 범위가 array[left, right]에 완전히 포함되는 경우
+		if(left <= nodeLeft && nodeRight <= right) 
+			return rangeMin[node];
+		// 양쪽 구간을 나눠서 푼 뒤 결과를 합친다.
+		int mid = (nodeLeft + nodeRight) / 2;
+		return min(query(left, right, node*2, nodeLeft, mid), query(left, right, node*2+1, mid+1, nodeRight));
+	}
+	
+	int query(int left, int right) {
+		return query(left, right, 1, 0, n-1);
+	}
 };
 
+
+
 int main() {
+	
 	int n;
 	cin >> n;
 	vector<int> list(n);
@@ -36,4 +58,7 @@ int main() {
 	for(int i=0; i<rmq->rangeMin.size(); ++i) {
 		cout << rmq->rangeMin[i] << " ";
 	}
+	cout << rmq->query(1,4) << endl;
+	
+	
 }
