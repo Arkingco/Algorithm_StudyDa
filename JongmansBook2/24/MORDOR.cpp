346c3ab34c3af9ec5636b22380fcca1db639b00f
박기선 <arkingco@gmail.com>
2020-12-26 05:42:46 +0000

    등산로 문제 아이게릿

--- JongmansBook2/24/MORDOR.cpp
+++ JongmansBook2/24/MORDOR.cpp
@@ -3,20 +3,16 @@
 using namespace std;
 
 const int INT_MAX = 987654321;
-
+const int INT_MIN = -1;
 struct RMQ {
-	// 배열의 길이
 	int n;
-	// 각 구간의 최소치
 	vector<int> rangeMin;
 	RMQ(const vector<int>& array) {
-		
 		n = array.size(); 
 		rangeMin.resize(n*4);
 		init(array, 0, n-1, 1);
 	}
 	int init(const vector<int>& array, int left, int right, int node) {
-		// 기저 사례
 		if(right == left) return rangeMin[node] = array[left];
 		int mid = (left + right) / 2;
 		int leftMin = init(array, left, mid, node*2);
@@ -24,15 +20,10 @@ struct RMQ {
 		return rangeMin[node] = min(leftMin, rightMin);
 	}
 	
-	// node가 표현하는 범위 array[nodeLeft, nodeRight] 가 주어질 때;
-	// 이 범위와 array[left, right]의 교집합의 최소치를 계산한다.
 	int query(int left, int right, int node, int nodeLeft, int nodeRight) {
-		// 두 구간이 겹치지 않으면 아주 큰 값을 반환한다. : 무시됨
 		if(right < nodeLeft || nodeRight < left) return INT_MAX;
-		// node가 표현하는 범위가 array[left, right]에 완전히 포함되는 경우
 		if(left <= nodeLeft && nodeRight <= right) 
 			return rangeMin[node];
-		// 양쪽 구간을 나눠서 푼 뒤 결과를 합친다.
 		int mid = (nodeLeft + nodeRight) / 2;
 		return min(query(left, right, node*2, nodeLeft, mid), query(left, right, node*2+1, mid+1, nodeRight));
 	}
@@ -42,22 +33,35 @@ struct RMQ {
 	}
 };
 
-
-
 int main() {
+	int C;
+	cin >> C;
 	
-	int n;
-	cin >> n;
-	vector<int> list(n);
-	for(int i=0; i<n; ++i ) {
-		cin >> list[i];
+	while(C--) {
+		int n, q;
+		cin >> n >> q;
+		
+		vector<int> list(n);
+		for(int i=0; i<n; ++i) {
+			cin >> list[i];	
+		}	
+		RMQ* rmq = new RMQ(list);
+		for(int i=0; i<n; ++i)
+			list[i] = list[i]*-1;
+		RMQ* rmqMax = new RMQ(list);
+		while(q--) {
+			int lowH, highH;
+			cin >> lowH >> highH;
+			
+			int minResult = rmq -> query(lowH, highH);
+			int maxResult = rmqMax -> query(lowH, highH);
+
+			cout << (maxResult + minResult) * -1 << endl;
+		}	
 	}
-	
-	RMQ* rmq = new RMQ(list);
-	
-	for(int i=0; i<rmq->rangeMin.size(); ++i) {
-		cout << rmq->rangeMin[i] << " ";
-	}
-	cout << rmq->query(1,4) << endl;
-	
 }
+
+
+
+
+
