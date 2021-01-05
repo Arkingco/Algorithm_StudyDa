ded51f1331d518a169cd04cbc944668c97438f64
박기선 <arkingco@gmail.com>
2021-01-05 06:09:44 +0000

    커밋

--- JongmansBook2/26/SOLONG.cpp
+++ JongmansBook2/26/SOLONG.cpp
@@ -1,48 +1,65 @@
 #include<iostream>
 #include<vector>
 #include<string>
+#include<cstring>
 using namespace std;
 
 const int ALPABET = 26;
 
-int charToNumber(char ch) { return 'A'-ch};
+int charToNumber(char ch) { return 'A'-ch;}
 
 struct Trie {
-	bool isterminal;
-	Trie* children[ALPABET];
 	
-	Trie() : isterminal(false) {
+	bool isEndWord;
+	Trie* children[ALPABET];
+	int HighProirityID;
+	
+	Trie() : isEndWord(false), HighProirityID(0) {
 		memset(children, 0, sizeof(children));
 	}
+
+	~Trie() {
+		for(int i=0; i<ALPABET; ++i) {
+			if(children[i] != NULL) {
+				delete children[i];
+			}
+		}
+	}
 	
-	void insert(const char* key) {
-		if(key* == 0) {
-			isterminal = true;
+	void insert(const char* key, int id) {
+
+		if(*key == 0) {
+			isEndWord = true;
 		} else {
-			int next = charToNumber(key*);
 			
-			if(children[next] == null) {
-				children[i] = new Trie();
+			if(HighProirityID <= id) {
+				HighProirityID = id;
 			}
 			
-			children[next] -> insert(key + 1);
+			int next = charToNumber(*key);
+			
+			if(children[next] == NULL) {
+				children[next] = new Trie();
+			}
+			
+			children[next]->insert(key + 1, HighProirityID);
 		}
 	}
 	
-	Trie* find(const char* key) {
-		if(key* == 0 ) {
-			return this;
+	bool find(const char* key) {
+		if(*key == 0 ) {
+			return true;
 		}
 		
-		int next = charToNumber(key*);
+		int next = charToNumber(*key);
 		if(children[next] == 0) {
-			return NULL;
+			return false;
 		}
 		
-		children[next] -> find(key+1);
+		return children[next] -> find(key+1);
 	}
 	
-}
+};
 
 int main() {
 	int T;
@@ -50,15 +67,29 @@ int main() {
 	while(T--) {
 		int n, m;
 		cin >> n >> m;
+		
+		Trie* trie = new Trie();
+		vector<string> wordList;
+		
+		
 		for(int i=0; i<n; ++i) {
-			string word;
-			int wordLength;
-			cin >> word >> wordLength;
+			string Trieword;
+			int wordPriority;
+			
+			cin >> Trieword >> wordPriority;	
+			trie->insert(Trieword.c_str(), wordPriority);
 		}
 		
 		for(int i=0; i<m; ++i) {
-			string moon;
-			cin >> moon;
+			string word;
+			cin >> word;
+			wordList.push_back(word);
 		}
+		
+		int ret = 0;
+		for(int i=0; i<wordList.size(); ++i) {
+			
+		}
+		
 	}
 }
