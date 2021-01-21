c92256df9fce39454e28d50c6314c5e7eff2384c
박기선 <arkingco@gmail.com>
2021-01-21 12:15:09 +0000

    오늘 공부 끝!

--- Baejun/KOI/1998/Elementary/2650.cpp
+++ Baejun/KOI/1998/Elementary/2650.cpp
@@ -4,28 +4,81 @@
 using namespace std;
 
 const int SQURD_MAP_MAX_SIZE = 4;
-int squrd_map[4] = {1, 3, 2, 4};
+int squrd_map[SQURD_MAP_MAX_SIZE+1] = {0, 1, 4, 2, 3};
+
+vector<int> wordList;
+int maxCrossSize = 0;
+int findWord(int word) {
+	for(int i=0; i<wordList.size(); ++i) {
+		if(word == wordList[i]) {
+			
+			int wordsize = wordList.size()-i;
+			maxCrossSize = max(maxCrossSize, wordsize);
+			wordList.erase(wordList.begin() + i);
+			
+			int ret = wordList.size()-i;
+			
+			return ret;
+		}
+	}
+	
+	return -1;
+}
 
 int main() {
-	vector<vector<int > > map = vector<vector<int > > (SQURD_MAP_MAX_SIZE + 1 , vector<int>(0,0));
+	
+	int map[51][51] = { 0, };
 	
 	int n;
 	cin >> n;
 	
-	for(int i=0; i<n; ++i) {
-		int a, b;
-		cin >> a >> b;
-		
-		map[a].push_back(b);
-	}
-	
-	for(int i=1; i<=4; ++i) {
-		sort(map[i].begin(), map[i].end());
-		for(int j=0; j<map[i].size(); ++j) {
-			cout << map[i][j] << " ";
+	for(int i=1; i<=n/2; ++i) {
+		for(int j=0; j<2; ++j) {
+			int a, b;
+			cin >> a >> b;
+			
+			map[a][b] = i;
 		}
-		cout << endl;
+		
+	}
+	
+	vector<int> list;
+	for(int i=1; i<=4; ++i) {
+		if(squrd_map[i]==2 || squrd_map[i] == 3) {
+			for(int j=n; j>=0; --j) {
+				if(map[squrd_map[i]][j] != 0 ) list.push_back(map[squrd_map[i]][j]);
+			}
+		} else {
+			for(int j=0; j<n; ++j) {
+				if(map[squrd_map[i]][j] != 0 ) list.push_back(map[squrd_map[i]][j]);
+			}
+		}
+		for(int z=0; z<list.size(); ++z) {
+		}
 	}
 	
 	
+	int ret = 0;
+	for(int i=0; i<list.size()-1; ++i) {
+		if(list[i] != list[i+1]) {
+			
+			int checkWord = findWord(list[i]);
+			if(checkWord != -1) {
+				ret = ret + checkWord;
+			} 
+			else {
+				wordList.push_back(list[i]);
+			}
+			
+		} else {
+			i++;
+		}
+	}
+	
+	for(int i=0; i<list.size(); ++i) {
+		cout << list[i] << " ";
+	}
+	cout << endl;
+	cout << ret << endl;
+	cout << maxCrossSize << endl;
 }
--- Baejun/ProgramSolving/Implement/14468.cpp
+++ Baejun/ProgramSolving/Implement/14468.cpp
@@ -24,13 +24,12 @@ int main() {
 	int ret = 0;
 	for(int i=0; i<a.size()-1; ++i) {
 		if(a[i] != a[i+1]) {
-			// 워드 리스트에 있다면 워드 리스트에서 빼주기
+			
 			int checkWord = findWord(a[i]);
 			if(checkWord != -1) {
 				
 				ret = ret + checkWord;
 			} 
-			// 워드 리스트에도 없다면 워드리스트에 추가하기
 			else {
 				wordList.push_back(a[i]);
 			}
