dd1932ae43354daadc790ac414da8348ecf4c6f3
arkingco <arkingco@gmail.com>
2021-03-03 22:49:06 +0900

    공부

---
+++ BAEKJUN_BOJ/SOLVED_CLASS/2(올클)/15829.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+#include<vector>
+#include<string>
+#include<math.h>
+#include<cstdio>
+using namespace std;
+
+const int charTOint = 96;
+const int moduler = 1234567891;
+int main()
+{
+	cout.tie(0); cin.tie(0); ios::sync_with_stdio(0);
+
+	
+}
